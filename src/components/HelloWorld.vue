1e5b8dfbeb8a70174ffc2e9892a883352ebcd83b
Sitthata <aprils96950@gmail.com>
2024-01-23 09:45:23 +0700

    init commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+interface Props {
+  msg: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
